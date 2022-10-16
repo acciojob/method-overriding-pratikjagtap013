1435214665b0344f83d89ebeeaf2976ccf9d1b59
Pratik Jagtap <pratikjagtap012@gmail.com>
2022-10-16 20:22:38 +0530

    updated test

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,5 +4,17 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    
+    private A a = new A();
+    private B b = new B();
+
+    @Test
+    public final void testprintHelloWorld() {
+        assertEquals("Invoking method from class A", a.meth());
+    }
+
+    @Test
+    public final void testprintHelloWorld2() {
+        assertEquals("Method is overridden in Extendend class B", b.meth());
+    }
+
 }
